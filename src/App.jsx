49d7e1353f8537efbcf0e7e83e0a8f39001c3b90
Victor Ruan Fabricio Cavalcante <victor.cavalcante@matriz.fornecedoramaquinas.com.br>
2024-12-30 07:48:39 -0300

    :fire: refactor(App): Removido código CSS e JSX não utilizado, implementada nova estrutura de layout com Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-screen h-screen bg-stone-100-site flex flex-col justify-center items-center'>
+
+        <div className='bg-branco-site w-[38rem] h-[33rem] rounded-3xl'>
+          
+          <div>
+            img
+          </div>
+          
+          <section>
+
+          </section>
+
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      
+      colors: {
+        "branco-site": 'var(--branco-site)',
+        "stone-100-site": 'var(--stone-100-site)',
+        "stone-150-site": 'var(--stone-150-site)',
+        "stone-600-site": 'var(--stone-600-site)',
+        "stone-900-site": 'var(--stone-900-site)',
+        "marrom-800-site": 'var(--marrom-800-site)',
+        "rose-800-site": 'var(--rose-800-site)',
+        "rose-50-site": 'var(--rose-50-site)'
+      },
+
+      fontFamily:{
+        'font-young': 'var(--font-young)',
+        'font-outfit': 'var(--font-outfit)'
+      }
     },
   },
   plugins: [],
